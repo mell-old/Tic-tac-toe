fd045a39f9b5c1f340f76da36efca1dcf2c2004a
Mell <mell-old@users.noreply.github.com>
2018-07-27 15:28:49 +0300

    Main.fin

--- Tic-tac-toe/mains.cpp
+++ Tic-tac-toe/mains.cpp
@@ -1,3 +1,31 @@
 #include "Header.h"
 
 int main()
+{
+	const int NumSquares = 9;
+	vector<char> board(NumSquares, EMPTY);
+	inctruction();
+	char human = humanPiece();
+	char computer = opponets(human);
+	char turn = X;
+	displayBoard(board);
+
+	while (winner(board) == NO_WIN)
+	{
+		int move;
+		if (turn == human)
+		{
+			move = humanMove(board, human);
+			board[move] = human;
+		}
+		else
+		{
+			move = computerMove(board, computer);
+			board[move] = human;
+		}
+		displayBoard(board);
+		turn = opponets(turn);
+	}
+	Win(winner(board), computer, human);
+	return 0;
+}
